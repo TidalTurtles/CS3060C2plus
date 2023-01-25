eb9039e929af67b754f05be046a68bffd4ea5e21
Noah Holt <tidalturtles@gmail.com>
2023-01-25 08:22:10 -0700

    more lab work
    finished first lab assignment and starting on the second

---
+++ hw1/inLab6_6/main.cpp
@@ -0,0 +1,52 @@
+/*
+ * Noah Holt
+ * CS 3060 C++
+ * Spring 23
+ *
+ * 6.6: In Lab test trial
+ */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//prototypes
+double kineticEnergy(double, double);
+
+int main()
+{
+
+    //vars
+    double mass;
+    double velocity;
+
+    //get mass
+    cout << "Enter the mass of the object: ";
+    cin >> mass;
+    cout << endl;
+
+    //get velocity
+    cout << "Enter the objects velocity: ";
+    cin >> velocity;
+    cout << endl;
+
+    //display kinetic
+    cout << "The Objects Kinetic Energy is " << "joules.\n";
+
+    return 0;
+
+}
+
+//kinetic energy function
+// KE = 1/2 mv^2
+double kineticEnergy(double mass, double velocity)
+{
+
+    double ke;
+
+    ke = .5 * mass * velocity * velocity;
+    // in class was return equation above
+
+    return ke;
+
+}
