a8d0ad88511e41588ab475920a6245153987fa89
Noah Holt <tidalturtles@gmail.com>
2023-01-25 08:06:16 -0700

    began hw2 and InLab1
    began homework 2 and in lab an wrote out what is needed

---
+++ hw/inLab6_6/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * Noah Holt
+ * CS 3060 C++
+ * Spring 23
+ *
+ * 6.6: In Lab test trial
+ */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+
+}
---
+++ hw2/hw7_3/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * Noah Holt
+ * CS 3060 C++
+ * Spring 23
+ * Due date: 30 Jan 23
+ *
+ * 7.3: Chips and Salsa
+ *
+ *          make two arrays (one for flavors and one for number sold)
+ *          iterate through to get the number sold per flavor
+ *          finally print out sales for all && total sales && name of highest and lowest sellers
+ *
+ */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+
+    cout << "Hello!!!";
+    return 0;
+
+}
+
