b12b60c53b6c750a7133367d97856ed1683421e7
Noah Holt <tidalturtles@gmail.com>
2023-01-25 09:12:38 -0700

    got secret code
    got secret example of possible on test

---
+++ hw1/iinLab6_20/main.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//prototype
+double profit(double, double, double);
+
+int main() {
+
+    //vars
+    int numShares;
+    double purchasePrice;
+    double purchaseCommission;
+    double salePrice;
+    double saleCommission;
+    double profit;
+
+    //get num shares
+    cout << "How many shares did you buy? ";
+    cin >> numShares;
+
+    //get purchase price of share
+    cout << "What was the purchace Price? ";
+    cin >> purchasePrice;
+
+    //get purchase commission
+
+    //get sales price
+
+    //get sales commission
+
+    //profit or loss
+
+    //display result
+
+    return 0;
+
+}
--- hw1/inlab6_18/main.cpp
+++ hw1/inlab6_18/main.cpp
@@ -1,6 +1,59 @@
+/*
+ * Noah Holt
+ * CS 3060 C++
+ * Spring 23
+ *
+ * 6.18 In lab
+ *
+ */
+
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+//prototypes
+int getNumberOfRooms();
+double getPaintPrice();
+double getWallArea();
+int numGallons(double);
+double laborHours(double);
+void displayCosts(int, double, double);
+
+//constants
+const int AREA_PER_GALLON = 110;
+const double LABOR_RATE = 25.0;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    //vars
+    int numRooms;
+    double paintPrice;
+    int totalWallArea = 0;
+    int gallons;
+    double hours;
+
+    //get num rooms
+    numRooms = getNumberOfRooms();
+
+    //get price of paint
+    paintPrice = getPaintPrice();
+
+    //get total area
+    for(int i = 0; i < numRooms; i++){
+        int wallArea;
+        cout << "Room " << i+1 << ":";
+        wallArea = getWallArea();
+        totalWallArea += wallArea;
+    }
+
+    //calc num gallons
+    gallons = numRooms(totalWallArea);
+
+    //calc labor hours
+
+    //display charges
+
     return 0;
 }
+
