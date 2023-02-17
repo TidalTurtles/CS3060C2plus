a2030b121c97f59512f88040735bb2fc03ed2f57
Noah Holt <tidalturtles@gmail.com>
2023-02-17 09:51:59 -0700

    moved
    moved to better directory and added last to header files that will be needed

---
+++ hw5/hw11_14/addNumbers.h
@@ -0,0 +1,21 @@
+//
+// Created by noah holt on 2/17/23.
+//
+
+#include <iostream>
+#include "binStruct.h"
+
+using namespace std;
+using namespace productBinStruct;
+
+#ifndef HW5_ADDNUMBERS_H
+#define HW5_ADDNUMBERS_H
+
+namespace addingParts
+{
+
+
+
+}
+
+#endif //HW5_ADDNUMBERS_H
---
+++ hw5/hw11_14/subtractNumbers.h
@@ -0,0 +1,21 @@
+//
+// Created by noah holt on 2/17/23.
+//
+
+#include <iostream>
+#include "binStruct.h"
+
+using namespace std;
+using namespace productBinStruct;
+
+#ifndef HW5_SUBTRACTNUMBERS_H
+#define HW5_SUBTRACTNUMBERS_H
+
+namespace deletingParts
+{
+
+
+
+}
+
+#endif //HW5_SUBTRACTNUMBERS_H
