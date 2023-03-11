149ae2ca91f975d822e1cd6be3a58253fbb5d5f5
Noah Holt <tidalturtles@gmail.com>
2023-03-11 10:46:39 -0700

    more labs
    A few more labs comploete

--- labs/lab4/lab10.12/cmake-build-debug/cmake_install.cmake
+++ labs/lab4/lab10.12/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/noah/Desktop/school/Spring 23/CS3060C2plus/labs/lab3/lab10.12
+# Install script for directory: /Users/noah/Desktop/school/Spring 23/CS3060C2plus/labs/lab4/lab10.12
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/noah/Desktop/school/Spring 23/CS3060C2plus/labs/lab3/lab10.12/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/noah/Desktop/school/Spring 23/CS3060C2plus/labs/lab4/lab10.12/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- labs/lab4/lab10.12/main.cpp
+++ labs/lab4/lab10.12/main.cpp
@@ -3,21 +3,24 @@
 //Probably on midterm ****
 
 #include <iostream>
+
+
 using namespace std;
+
 
 //global constants
 const int SIZE = 80;
 const int MIN = 6;
 
 //funcs
-bool hasLength(char[], int minimum);
+bool hasLength(char[]);
 bool hasUpper(char[]);
 bool hasLower(char[]);
 bool hasDigit(char[]);
 bool isValid(char[]);
 
 void displayResults(char[]);
-void displayRequirements(char[]);
+void displayRequirements();
 
 //other file displayRequirements
 
@@ -25,12 +28,7 @@ int main() {
 
     char cstring[SIZE];
 
-    //display requirements
-        //output requirements for password
-        //min length
-        //one uppeer
-        //one lower
-        //one digit
+    displayRequirements();
 
     //get input
     cout << "Enter Password: ";
@@ -38,12 +36,154 @@ int main() {
 
     //display results
     displayResults(cstring);
-        //make if valid
-            //good
-        //else
-            //check wrong
-            //print all
 
     return 0;
+
+}
+
+void displayRequirements()
+{
+    //BIG cout coming up.
+    cout << "Password Requirements:" << endl;
+    cout << "The Password should be at least " << MIN << " characters long!" << endl;
+    cout << "The Password should contain at least one uppercase!" << endl;
+    cout << "The Password should contain at least one lowercase!" << endl;
+    cout << "The Password should contain at least one digit!" << endl;
+}
+
+bool hasLength(char str[])
+{
+
+    bool status = false;
+    int count = 0;
+
+    while(*str != '\0'){
+        count++;
+        *str++;
+    }
+
+    if(count >= MIN){
+        status = true;
+    }
+
+    return status;
+
+}
+
+bool hasUpper(char str[])
+{
+
+    bool status = false;
+    int count = 0;
+
+    while(*str != '\0' && count == 0){
+
+        if(isupper(*str)){
+            count++;
+        }
+
+        *str++;
+
+    }
+
+    if(count > 0){
+        status = true;
+    }
+
+    return status;
+
+}
+
+bool hasLower(char str[])
+{
+
+    bool status = false;
+    int count = 0;
+
+    while(*str != '\0' && count == 0){
+
+        if(islower(*str)){
+            count++;
+        }
+
+        *str++;
+
+    }
+
+    if(count > 0){
+        status = true;
+    }
+
+    return status;
+
+}
+
+bool hasDigit(char str[])
+{
+
+    bool status = false;
+    int count = 0;
+
+    while(*str != '\0' && count == 0){
+
+        if(isdigit(*str)){
+            count++;
+        }
+
+        *str++;
+
+    }
+
+    if(count > 0){
+        status = true;
+    }
+
+    return status;
+
+}
+
+//could be char* str[], char* str, char str[],
+//first two are pointers
+// if just array last char does not need to be \0
+bool isValid(char str[])
+{
+
+    bool status = false;
+
+    if (hasLength(str) && hasLower(str) && hasUpper(str) && hasDigit(str)){
+        status = true;
+    }
+
+    return status;
+
+}
+
+void displayResults(char str[])
+{
+
+    if(isValid(str)){
+        cout << "\nPassword is Valid!\n";
+    } else {
+
+        cout << "\nPassword is Invalid!!\n";
+
+        //why invalid?
+        if(!hasLength(str)){
+            cout << "Password should be at LEAST " << MIN << " characters long!\n";
+        }
+
+        if(!hasLower(str)){
+            cout << "Password should have at least ONE lowercase letter!\n";
+        }
+
+        if(!hasUpper(str)){
+            cout << "Password should have at least ONE uppercase letter!\n";
+        }
+
+        if(!hasDigit(str)){
+            cout << "Password should have at least ONE digit!\n";
+        }
+
+    }
 
 }
