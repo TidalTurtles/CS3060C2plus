6dba40f58a373c45491a54518086b3b65f74777d
Noah Holt <tidalturtles@gmail.com>
2023-04-15 13:51:20 -0600

    hw 10
    began homework ten which is 17.6

---
+++ hw10/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * Noah Holt
+ * CS 3060 C++
+ * Spring 23
+ * Due date: 16 april 23
+ *
+ * 17.6: Word Frequency
+ *
+ *      Read text file
+ *      make a map
+ *          key: word
+ *          value: count
+ *
+ *      Either Print
+ *      OR
+ *      print to file
+ *
+ */
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+
+}
