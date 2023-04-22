bfe324582443e4874db2a6976d0c151665bb9c15
Noah Holt <tidalturtles@gmail.com>
2023-04-22 16:26:29 -0600

    starting hw 11
    starting homework 11
    
    most is in textbook so hoping it won't take long.

---
+++ hw11/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * Noah Holt
+ * CS 3060 C++
+ * Spring 23
+ * Due date: 24 april 23
+ *
+ * 21.1: Binary Tree Template
+ *
+ *      make tree for any data type
+ *
+ *      show working with main
+ *
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
